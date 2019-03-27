d45a338e41483b37684023360412c9f895a4d94a
Hristo Kolev <hristo.a.kolev@gmail.com>
2019-03-27 19:48:12 +0200

    DTO generation

--- server/src/Newsgirl.WebServices/Infrastructure/RpcCodeGenerator.cs
+++ server/src/Newsgirl.WebServices/Infrastructure/RpcCodeGenerator.cs
@@ -17,18 +17,71 @@ namespace Newsgirl.WebServices.Infrastructure
                                 .Where(x => x != null)
                                 .ToList();
 
+            var allTypes = GetAllTypes(types);
+
+            foreach (Type type in allTypes)
+            {
+                MainLogger.Instance.LogDebug($"TYPE: {type.Name}");
+            }
+
+            return 0;
+        }
+
+        private static HashSet<Type> GetAllTypes(List<Type> types)
+        {
+            var bannedTypes = new[]
+            {
+                typeof(byte),
+                typeof(sbyte),
+                typeof(short),
+                typeof(ushort),
+                typeof(uint),
+                typeof(ulong),
+                typeof(float),
+                typeof(char),
+                typeof(double),
+            };
+
             var simpleTypes = new[]
             {
                 typeof(int),
                 typeof(long),
-                typeof(decimal),
                 typeof(string),
+                typeof(bool),
+                typeof(decimal),
+                typeof(DateTime)
             };
 
             var allTypes = new HashSet<Type>();
 
             void Recurse(Type targetType)
             {
+                if (bannedTypes.Contains(targetType))
+                {
+                    throw new DetailedLogException("Banned type detected in one of the DTO classes.")
+                    {
+                        Context =
+                        {
+                            {"TypeName", targetType.Name},
+                        }
+                    };
+                }
+
+                if (simpleTypes.Contains(targetType))
+                {
+                    return;
+                }
+
+                if (targetType.IsGenericType)
+                {
+                    foreach (var t in targetType.GetGenericArguments())
+                    {
+                        Recurse(t);
+                    }
+
+                    return;
+                }
+
                 if (allTypes.Add(targetType))
                 {
                     var props = targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
@@ -45,12 +98,7 @@ namespace Newsgirl.WebServices.Infrastructure
                 Recurse(type);
             }
 
-            foreach (Type type in allTypes)
-            {
-                MainLogger.Instance.LogDebug($"TYPE: {type.Name}");
-            }
-
-            return 0;
+            return allTypes;
         }
     }
 }
