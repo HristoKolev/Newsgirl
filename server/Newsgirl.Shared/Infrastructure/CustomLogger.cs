9bc6988fefff2d39aed742259ad5a7602baf452e
Hristo Kolev <hristo.a.kolev@gmail.com>
2020-03-06 23:39:39 +0200

    logging part 1

---
+++ server/Newsgirl.Shared/Infrastructure/CustomLogger.cs
@@ -0,0 +1,353 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Sentry;
+using Sentry.Protocol;
+
+namespace Newsgirl.Shared.Infrastructure
+{
+    public class CustomLogger
+    {
+        private readonly CustomLoggerConfig config;
+        private readonly ISentryClient sentryClient;
+        private readonly AsyncLock sentryFlushLock = new AsyncLock();
+        private readonly TimeSpan sentryFlushTimeout;
+
+        public CustomLogger(CustomLoggerConfig config)
+        {
+            this.config = config;
+            this.sentryFlushTimeout = TimeSpan.FromSeconds(60);
+            this.sentryClient = new SentryClient(new SentryOptions
+            {
+                AttachStacktrace = true,
+                Dsn = new Dsn(config.SentryDsn),
+            });
+        }
+        
+        public void Debug(string message)
+        {
+            if (!this.config.EnableDebug)
+            {
+                return;
+            }
+
+            this.Log(message);
+        }
+        
+        public void Log(string message)
+        {
+            if (!this.config.DisableConsoleLogging)
+            {
+                Console.Out.WriteLine(message);
+            }
+        }
+
+        public async Task<string> Error(Exception exception, Dictionary<string, object> additionalInfo = null)
+        {
+            if (!this.config.DisableConsoleLogging)
+            {
+                Console.Error.WriteLine(exception.ToString());
+            }
+
+            if (!this.config.DisableSentryIntegration)
+            {
+                return await this.SendToSentry(exception, additionalInfo);
+            }
+
+            return null;
+        }
+
+        private async Task<string> SendToSentry(Exception exception, Dictionary<string, object> additionalInfo)
+        {
+            var exceptionList = GetExceptionChain(exception);
+
+            var contextEntries = exceptionList.Where(x => x is DetailedLogException)
+                                     .Cast<DetailedLogException>()
+                                     .SelectMany(x => x.Details)
+                                     .ToList();
+
+            if (additionalInfo != null)
+            {
+                contextEntries.AddRange(additionalInfo);
+            }
+
+            var sentryEvent = new SentryEvent(exception)
+            {
+                Level = SentryLevel.Error,
+            };
+
+            foreach (var entry in contextEntries)
+            {
+                sentryEvent.SetExtra(entry.Key, entry.Value);
+            }
+
+            string customFingerprint = exceptionList.Where(x => x is DetailedLogException)
+                                                   .Cast<DetailedLogException>()
+                                                   .Select(x => x.Fingerprint)
+                                                   .FirstOrDefault(x => !string.IsNullOrWhiteSpace(x));
+            if (customFingerprint != null)
+            {
+                sentryEvent.SetFingerprint(new[] { customFingerprint });
+            }
+            else
+            {
+                sentryEvent.SetFingerprint(exceptionList.Select(GetFingerprint));
+            }
+
+            sentryEvent.ServerName = this.config.ServerName;
+
+            if (!string.IsNullOrWhiteSpace(this.config.Environment))
+            {
+                sentryEvent.Environment = this.config.Environment;
+            }
+
+            var eventId = this.sentryClient.CaptureEvent(sentryEvent);
+
+            if (!this.config.DisableSentryAutoFlush)
+            {
+                using (await this.sentryFlushLock.Lock())
+                {
+                    await this.sentryClient.FlushAsync(this.sentryFlushTimeout);
+                }
+            }
+            
+            return eventId.ToString();
+        }
+        
+        /// <summary>
+        /// Returns a flat list of all the exceptions down the .InnerException chain.
+        /// </summary>
+        private static List<Exception> GetExceptionChain(Exception exception)
+        {
+            var list = new List<Exception>();
+
+            while (exception != null)
+            {
+                list.Add(exception);
+                exception = exception.InnerException;
+            }
+
+            return list;
+        }
+
+        private static string GetFingerprint(Exception exception)
+        {
+            var stackTrace = SentryStackTraceFactory.Create(exception);
+
+            string frames = string.Join("\n", stackTrace.Frames.Select(frame => $"{frame.Module} => {frame.Function}"));
+
+            return $"[{exception.GetType()}]\n{frames}";
+        }
+    }
+
+    public class CustomLoggerConfig
+    {
+        public bool DisableSentryIntegration { get; set; }
+
+        public bool DisableSentryAutoFlush { get; set; }
+        
+        public bool DisableConsoleLogging { get; set; }
+
+        public bool EnableDebug { get; set; }
+        
+        public string ServerName { get; set; }
+        
+        public string Environment { get; set; }
+        
+        public string SentryDsn { get; set; }
+    }
+
+    /// <summary>
+    /// Stolen from Sentry's source code.
+    /// </summary>
+    public static class SentryStackTraceFactory
+    {
+        private static readonly List<(string, string)> IgnoredFrames = new List<(string, string)>
+        {
+            ("System.Runtime.CompilerServices.TaskAwaiter", "GetResult"),
+            ("System.Runtime.CompilerServices.TaskAwaiter`1", "GetResult"),
+
+            ("System.Runtime.CompilerServices.TaskAwaiter", "HandleNonSuccessAndDebuggerNotification"),
+            ("System.Runtime.CompilerServices.TaskAwaiter`1", "HandleNonSuccessAndDebuggerNotification"),
+
+            ("System.Runtime.CompilerServices.TaskAwaiter", "ThrowForNonSuccess"),
+            ("System.Runtime.CompilerServices.TaskAwaiter`1", "ThrowForNonSuccess"),
+
+            ("System.Runtime.ExceptionServices.ExceptionDispatchInfo", "Throw"),
+
+            ("System.Runtime.CompilerServices.ValueTaskAwaiter`1", "GetResult"),
+            ("System.Threading.Tasks.ValueTask`1", "get_Result"),
+        };
+
+        public static SentryStackTrace Create(Exception exception)
+        {
+            return Create(new StackTrace(exception, true));
+        }
+
+        private static SentryStackTrace Create(StackTrace stackTrace)
+        {
+            var frames = CreateFrames(stackTrace).Reverse();
+
+            var stacktrace = new SentryStackTrace();
+
+            foreach (var frame in frames)
+            {
+                if (IgnoredFrames.All(tuple => frame.Module != tuple.Item1 || frame.Function != tuple.Item2))
+                {
+                    stacktrace.Frames.Add(frame);
+                }
+            }
+
+            return stacktrace;
+        }
+
+        private static IEnumerable<SentryStackFrame> CreateFrames(StackTrace stackTrace)
+        {
+            var frames = stackTrace?.GetFrames();
+
+            if (frames == null)
+            {
+                yield break;
+            }
+
+            var firstFrames = true;
+            foreach (var stackFrame in frames)
+            {
+                // Remove the frames until the call for capture with the SDK
+                if (firstFrames
+                    // ReSharper disable once PatternAlwaysOfType
+                    && stackFrame.GetMethod() is MethodBase method
+                    && method.DeclaringType?.AssemblyQualifiedName?.StartsWith("Sentry") == true)
+                {
+                    continue;
+                }
+
+                firstFrames = false;
+
+                var frame = InternalCreateFrame(stackFrame);
+                if (frame != null)
+                {
+                    yield return frame;
+                }
+            }
+        }
+
+        private static SentryStackFrame InternalCreateFrame(StackFrame stackFrame)
+        {
+            const string unknownRequiredField = "(unknown)";
+
+            var frame = new SentryStackFrame();
+
+            // ReSharper disable once PatternAlwaysOfType
+            if (stackFrame.GetMethod() is MethodBase method)
+            {
+                // TODO: SentryStackFrame.TryParse and skip frame instead of these unknown values:
+                frame.Module = method.DeclaringType?.FullName ?? unknownRequiredField;
+                frame.Package = method.DeclaringType?.Assembly.FullName;
+                frame.Function = method.Name;
+            }
+
+            frame.InApp = true;
+            frame.FileName = stackFrame.GetFileName();
+
+            // stackFrame.HasILOffset() throws NotImplemented on Mono 5.12
+            var ilOffset = stackFrame.GetILOffset();
+
+            if (ilOffset != 0)
+            {
+                frame.InstructionOffset = stackFrame.GetILOffset();
+            }
+
+            var lineNo = stackFrame.GetFileLineNumber();
+            if (lineNo != 0)
+            {
+                frame.LineNumber = lineNo;
+            }
+
+            var colNo = stackFrame.GetFileColumnNumber();
+
+            if (lineNo != 0)
+            {
+                frame.ColumnNumber = colNo;
+            }
+
+            DemangleAsyncFunctionName(frame);
+            DemangleAnonymousFunction(frame);
+
+            return frame;
+        }
+
+        private static void DemangleAsyncFunctionName(SentryStackFrame frame)
+        {
+            if (frame.Module == null || frame.Function != "MoveNext")
+            {
+                return;
+            }
+
+            //  Search for the function name in angle brackets followed by d__<digits>.
+            //
+            // Change:
+            //   RemotePrinterService+<UpdateNotification>d__24 in MoveNext at line 457:13
+            // to:
+            //   RemotePrinterService in UpdateNotification at line 457:13
+
+            var match = Regex.Match(frame.Module, @"^(.*)\+<(\w*)>d__\d*$");
+            if (match.Success && match.Groups.Count == 3)
+            {
+                frame.Module = match.Groups[1].Value;
+                frame.Function = match.Groups[2].Value;
+            }
+        }
+
+        private static void DemangleAnonymousFunction(SentryStackFrame frame)
+        {
+            if (frame?.Function == null)
+            {
+                return;
+            }
+
+            // Search for the function name in angle brackets followed by b__<digits/letters>.
+            //
+            // Change:
+            //   <BeginInvokeAsynchronousActionMethod>b__36
+            // to:
+            //   BeginInvokeAsynchronousActionMethod { <lambda> }
+
+            var match = Regex.Match(frame.Function, @"^<(\w*)>b__\w+$");
+            if (match.Success && match.Groups.Count == 2)
+            {
+                frame.Function = match.Groups[1].Value + " { <lambda> }";
+            }
+        }
+    }
+    
+    public class DetailedLogException : Exception
+    {
+        public DetailedLogException()
+        {
+            this.Details = new Dictionary<string, object>();
+        }
+
+        public DetailedLogException(string message)
+            : base(message)
+        {
+            this.Details = new Dictionary<string, object>();
+        }
+
+        public DetailedLogException(string message, Exception inner)
+            : base(message, inner)
+        {
+            this.Details = new Dictionary<string, object>();
+        }
+
+        // ReSharper disable once CollectionNeverUpdated.Global
+        public Dictionary<string, object> Details { get; }
+        
+        public string Fingerprint { get; set; }
+    }
+}
