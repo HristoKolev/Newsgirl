022d4dea765796d0d116d925b9519e39e3c6252d
Hristo Kolev <hristo.a.kolev@gmail.com>
2020-03-29 16:50:14 +0300

    middleware benchmark

--- server/research/RpcPlay/Program.cs
+++ server/research/RpcPlay/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Threading.Tasks;
@@ -47,80 +46,15 @@ namespace RpcPlay
 
         public static async Task Main()
         {
+            await IlGeneratedCode();
             
-     
-            // await CompiledExpressions();
-            //
-            // await IlGeneratedCode();
-            //
-            // await InlinedCode();
-            //
-            // Console.WriteLine("===================");
-            //
-            // await CompiledExpressions();
-            //
-            // await IlGeneratedCode();
-            //
-            // await InlinedCode();
+            await InlinedCode();
             
-            var context = new RpcContext
-            {
-                Request = new RequestModel(),
-                InstanceProvider = new InstanceProvider(),
-            };
+            Console.WriteLine("===================");
 
-            var midTypes = new[]
-            {
-                typeof(Mid1),
-                typeof(Mid2),
-                typeof(Mid3),
-                typeof(Mid4),
-            };
-
-            // force create
-            foreach (var type in midTypes)
-            {
-                context.InstanceProvider.Get(type);
-            }
-
-            var getInstanceProvider = typeof(RpcContext).GetProperty("InstanceProvider").GetMethod;
-            var getInstance = typeof(InstanceProvider).GetMethod("Get");
-
-            var typeBuilder = IlGeneratorHelper.ModuleBuilder.DefineType("RpcMiddlewareDynamicType+" + Guid.NewGuid(),
-                TypeAttributes.Class | TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.Abstract);
-
-            MethodInfo lastMethod = typeof(Example).GetMethod("RunCode");
-
-            for (int i = midTypes.Length - 1; i >= 0; i--)
-            {
-                var midType = midTypes[i];
-                
-                var currentMethod = typeBuilder.DefineMethod(
-                    "mid" + i,
-                    MethodAttributes.Private | MethodAttributes.Static,
-                    typeof(Task),
-                    new[] {typeof(RpcContext)}
-                );
-
-                var gen = currentMethod.GetILGenerator();
-
-                gen.Emit(OpCodes.Ldarg_0);
-                gen.Emit(OpCodes.Call, getInstanceProvider);
-                gen.Emit(OpCodes.Ldtoken, midType);
-                gen.Emit(OpCodes.Call, getInstance);
-                gen.Emit(OpCodes.Ldarg_0);
-                gen.LoadDelegate<RpcRequestDelegate>(lastMethod);
-                gen.Emit(OpCodes.Call, midType.GetMethod("Run"));
-                gen.Emit(OpCodes.Ret);
-
-                lastMethod = currentMethod;
-            }
+            await IlGeneratedCode();
             
-            var dynamicType = typeBuilder.CreateType();
-            var func = (RpcRequestDelegate) dynamicType.GetMethod("mid0", BindingFlags.NonPublic | BindingFlags.Static)
-                .CreateDelegate(typeof(RpcRequestDelegate));
-
-            await func(context);
+            await InlinedCode();
         }
 
         private static async Task IlGeneratedCode()
@@ -236,120 +170,47 @@ namespace RpcPlay
                         .Run(ctx3, ctx2 => ((Mid1) ctx2.InstanceProvider.Get(typeof(Mid1)))
                             .Run(ctx2, rpcContext => RunCode(rpcContext)))));
         }
-
-        private static async Task CompiledExpressions()
-        {
-            var context = new RpcContext
-            {
-                Request = new RequestModel(),
-                InstanceProvider = new InstanceProvider(),
-            };
-
-            var midTypes = new[]
-            {
-                typeof(Mid1),
-                typeof(Mid2),
-                typeof(Mid3),
-                typeof(Mid4),
-            };
-
-            // force create
-            foreach (var type in midTypes)
-            {
-                context.InstanceProvider.Get(type);
-            }
-
-            var getInstance = typeof(InstanceProvider).GetMethod("Get");
-
-            var contextParam = Expression.Parameter(typeof(RpcContext), "context");
-
-            var lambdaExpression = Expression.Lambda<RpcRequestDelegate>(
-                Expression.Call(typeof(Example).GetMethod("RunCode"), contextParam), contextParam);
-
-            for (int i = midTypes.Length - 1; i >= 0; i--)
-            {
-                var midType = midTypes[i];
-
-                var localContextParam = Expression.Parameter(typeof(RpcContext), "context" + i);
-                var instanceProviderExpr = Expression.Property(localContextParam, "InstanceProvider");
-                var getCall = Expression.Call(instanceProviderExpr, getInstance, Expression.Constant(midType));
-
-                var localBody = Expression.Call(
-                    Expression.Convert(getCall, midType),
-                    midType.GetMethod("Run"),
-                    localContextParam,
-                    lambdaExpression
-                );
-
-                lambdaExpression = Expression.Lambda<RpcRequestDelegate>(localBody, localContextParam);
-            }
-
-            var func = lambdaExpression.Compile();
-
-            var sw = Stopwatch.StartNew();
-
-            for (int i = 0; i < Example.IterationCount; i++)
-            {
-                await func(context);
-            }
-            
-            sw.Stop();
-            
-            Console.WriteLine($"COMPILED FUNC: {sw.ElapsedMilliseconds}");
-        }
-
+ 
         public static Task RunCode(RpcContext context)
         {
-            Console.WriteLine("RUN CODE...");
+            Example.Cats += 1;
             return Task.CompletedTask;
         }
     }
 
     public class Mid1 : IMid
     {
-        public async Task Run(RpcContext context, RpcRequestDelegate next)
+        public Task Run(RpcContext context, RpcRequestDelegate next)
         {
-            Console.WriteLine(1);
-
-            await next(context);
-            
-            Console.WriteLine(8);
+            Example.Cats += 1;
+            return next(context);
         }
     }
     
     public class Mid2 : IMid
     {
-        public async Task Run(RpcContext context, RpcRequestDelegate next)
+        public Task Run(RpcContext context, RpcRequestDelegate next)
         {
-            Console.WriteLine(2);
-
-            await next(context);
-            
-            Console.WriteLine(7);
+            Example.Cats += 1;
+            return next(context);
         }
     }
     
     public class Mid3 : IMid
     {
-        public async Task Run(RpcContext context, RpcRequestDelegate next)
+        public Task Run(RpcContext context, RpcRequestDelegate next)
         {
-            Console.WriteLine(3);
-
-            await next(context);
-            
-            Console.WriteLine(6);
+            Example.Cats += 1;
+            return next(context);
         }
     }
     
     public class Mid4 : IMid
     {
-        public async Task Run(RpcContext context, RpcRequestDelegate next)
+        public Task Run(RpcContext context, RpcRequestDelegate next)
         {
-            Console.WriteLine(4);
-
-            await next(context);
-            
-            Console.WriteLine(5);
+            Example.Cats += 1;
+            return next(context);
         }
     }
 
