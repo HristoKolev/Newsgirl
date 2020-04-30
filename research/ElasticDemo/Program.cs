751adddfeeb2058cdd98c417ea12e003fe8dfe25
Hristo Kolev <hristo.a.kolev@gmail.com>
2020-04-30 20:37:14 +0300

    elastic demo

---
+++ research/ElasticDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ElasticDemo
+{
+    using System.Threading.Tasks;
+    using Elasticsearch.Net;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var node = new Uri("http://192.168.0.107:9200");
+                var config = new ConnectionConfiguration(node);
+                config.BasicAuthentication("elastic", "changeme");
+                var client = new ElasticLowLevelClient(config);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    var ss = await client.IndexAsync<MyModel>("hristo_logs", "{\"cat_id\": " + i + "}");
+
+                    Console.WriteLine(ss);   
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    class MyModel : ElasticsearchResponseBase
+    {
+    }
+}
