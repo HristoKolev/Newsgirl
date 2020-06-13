05db61c89d992fe11cbccb1fe441d85c1244f6a8
Hristo Kolev <hristo.a.kolev@gmail.com>
2020-06-13 21:13:39 +0300

    elk client part 2

--- server/src/Newsgirl.Shared/Logging/Consumers/ElasticsearchLogConsumer.cs
+++ server/src/Newsgirl.Shared/Logging/Consumers/ElasticsearchLogConsumer.cs
@@ -1,7 +1,11 @@
 namespace Newsgirl.Shared.Logging.Consumers
 {
     using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Text.Json;
     using System.Threading.Tasks;
     using Logging;
@@ -17,21 +21,26 @@ namespace Newsgirl.Shared.Logging.Consumers
             this.elasticsearchClient = new ElasticsearchClient(config);
         }
         
-        protected override   ValueTask Flush(ArraySegment<LogData> data)
+        protected override ValueTask Flush(ArraySegment<LogData> data)
         {
-            // for (int i = 0; i < data.Count; i++)
-            // {
-            //     string jsonBody = JsonSerializer.Serialize(data[i].Fields);
-            //
-            //     var response = await this.elasticsearchClient.IndexAsync<CustomElasticsearchResponse>(this.indexName, jsonBody);
-            //
-            //     if (!response.Success)
-            //     {
-            //         throw new ApplicationException(response.ToString());
-            //     }
-            // }
-            
-            return new ValueTask();
+            var array = ArrayPool<Dictionary<string, object>>.Shared.Rent(data.Count);
+
+            try
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    array[i] = data[i].Fields;
+                }
+
+                return this.elasticsearchClient.BulkCreate(
+                    this.indexName,
+                    new ArraySegment<Dictionary<string, object>>(array, 0, data.Count)
+                );
+            }
+            finally
+            {
+                ArrayPool<Dictionary<string, object>>.Shared.Return(array);
+            }
         }
     }
     
@@ -46,21 +55,9 @@ namespace Newsgirl.Shared.Logging.Consumers
             this.elasticsearchClient = new ElasticsearchClient(config);
         }
         
-        protected override   ValueTask Flush(ArraySegment<T> data)
+        protected override ValueTask Flush(ArraySegment<T> data)
         {
-            // for (int i = 0; i < data.Count; i++)
-            // {
-            //     string jsonBody = JsonSerializer.Serialize(data[i]);
-            //
-            //     var response = await this.elasticsearchClient.IndexAsync<CustomElasticsearchResponse>(this.indexName, jsonBody);
-            //
-            //     if (!response.Success)
-            //     {
-            //         throw new ApplicationException(response.ToString());
-            //     }
-            // }
-            
-            return new ValueTask();
+            return this.elasticsearchClient.BulkCreate(this.indexName, data);
         }
     }
     
@@ -74,24 +71,88 @@ namespace Newsgirl.Shared.Logging.Consumers
         public string Password { get; set; }
     }
 
+    /// <summary>
+    /// A simple client for elasticsearch.
+    /// </summary>
     public class ElasticsearchClient
     {
         private readonly HttpClient httpClient;
-        private readonly ElasticsearchConfig config;
+        private readonly byte[] bulkHeaderBytes = EncodingHelper.UTF8.GetBytes("{\"create\":{}}\n");
+        private readonly byte[] bulkNewLineBytes = EncodingHelper.UTF8.GetBytes("\n");
+        private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
 
         public ElasticsearchClient(HttpClient httpClient, ElasticsearchConfig config)
         {
             this.httpClient = httpClient;
-            this.config = config;
+            
+            httpClient.Timeout = TimeSpan.FromMinutes(1);
+            httpClient.BaseAddress = new Uri(config.Url);
+
+            string basicToken = Convert.ToBase64String(
+                EncodingHelper.UTF8.GetBytes($"{config.Username}:{config.Password}")
+            );
+            
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicToken);
         }
 
         public ElasticsearchClient(ElasticsearchConfig config) : this(new HttpClient(), config)
         {
         }
 
-        public Task Index(string index)
+        public async ValueTask BulkCreate<T>(string index, ArraySegment<T> data)
         {
-            return Task.CompletedTask;
+            await using var memStream = new MemoryStream();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                await memStream.WriteAsync(this.bulkHeaderBytes);
+                await JsonSerializer.SerializeAsync(memStream, data[i]);
+                await memStream.WriteAsync(this.bulkNewLineBytes);
+            }
+
+            memStream.Position = 0;
+
+            var content = new StreamContent(memStream);
+            
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            
+            var response = await this.httpClient.PostAsync(
+                new Uri(index + "/_bulk", UriKind.Relative),
+                content
+            );
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new DetailedLogException("Elasticsearch endpoint returned a non-success status code.")
+                {
+                    Details =
+                    {
+                        {"elasticsearchResponseStatusCode", (int)response.StatusCode},
+                        {"elasticsearchResponseJson", responseBody},                    
+                    }
+                };    
+            }
+            
+            var responseDto = JsonSerializer.Deserialize<ElasticsearchBulkResponse>(responseBody, JsonSerializerOptions);
+
+            if (responseDto.Errors)
+            {
+                throw new DetailedLogException("Elasticsearch endpoint returned an error.")
+                {
+                    Details =
+                    {
+                        {"elasticsearchResponseJson", responseBody},                    
+                    }
+                };
+            }
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class ElasticsearchBulkResponse
+        {
+            public bool Errors { get; set; }
         }
     }
 }
