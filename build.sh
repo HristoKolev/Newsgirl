1e12c0e739f97cc452810cfab9219f699ac060cc
Hristo Kolev <hristo.a.kolev@gmail.com>
2022-01-23 00:05:26 +0200

    Adjusted build scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
-export DOCKER_BUILDKIT=1
+TMP_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$TMP_SOURCE" ]; do
+  SCRIPT_PATH="$( cd -P "$( dirname "$TMP_SOURCE" )" >/dev/null 2>&1 && pwd )"
+  TMP_SOURCE="$(readlink "$TMP_SOURCE")"
+  [[ $TMP_SOURCE != /* ]] && TMP_SOURCE="$SCRIPT_PATH/$TMP_SOURCE"
+done
+SCRIPT_PATH="$( cd -P "$( dirname "$TMP_SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-# tarball csproj files, sln files, and NuGet.config
-find . \( -name "*.csproj" -o -name "*.sln" -o -name "NuGet.config" \) -print0 \
-    | tar -cvf ./projectfiles.tar --null -T -
+set -eu -o pipefail
 
-docker build -f ./FetcherDockerfile -t xdxd-registry.lan/newsgirl-fetcher .
+# We want to cache the package restore steps when running docker build.
+# This is achieved by having a restore step at the start of each Dockerfile that
+# only includes files that are required to restore packages so that that step
+# only runs if these files change.
+#
+# We do this by packaging these files in a tar archive and unpacking it in the build step.
+#
+# Currently these types of files are included:
+#   *.csproj
+#   *.sln
 
 rm ./projectfiles.tar -f
+find . \( -name "*.csproj" -o -name "*.sln" \) -print0 | tar -cvf ./projectfiles.tar --null -T -
 
+export DOCKER_BUILDKIT=1
+
+docker build -f ./FetcherDockerfile -t xdxd-registry.lan/newsgirl-fetcher .
 docker push xdxd-registry.lan/newsgirl-fetcher
+
+rm ./projectfiles.tar -f
